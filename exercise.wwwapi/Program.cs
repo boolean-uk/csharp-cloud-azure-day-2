a126a49551742fe45a62f8f11c22a5870917bcab
JHalvor <jonashalvorsen2013@live.no>
2024-10-25 09:25:47 +0200

    Exercise-complete

---
+++ exercise.wwwapi/Program.cs
@@ -0,0 +1,45 @@
+using exercise.wwwapi.Data;
+using exercise.wwwapi.Repositories;
+using Microsoft.EntityFrameworkCore;
+using exercise.wwwapi.Models;
+using exercise.wwwapi.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("ProductDb"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+/*Exception handling*/
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next(context);
+    }
+    catch (BadHttpRequestException ex)
+    {
+        var exceptionMessage = ex.Message;
+        await Results.BadRequest($"Price must be an integer, something else was provided").ExecuteAsync(context);
+    }
+});
+
+app.ConfigureProductEndpoint();
+
+app.Run();
+
